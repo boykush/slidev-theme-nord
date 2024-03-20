da5a65ed84d109778e93d0662e72c928084c7114
Taichi Kushiro <k.taichi0315@gmail.com>
2024-03-20 18:26:00 +0900

    fix marmaid left layout

--- example.md
+++ example.md
@@ -78,21 +78,15 @@ layout: mermaid-left
 # Mermaid
 
 ```mermaid
-block-beta
-columns 1
-  db(("DB"))
-  blockArrowId6<["&nbsp;&nbsp;&nbsp;"]>(down)
-  block:ID
-    A
-    B["A wide one in the middle"]
-    C
-  end
-  space
-  D
-  ID --> D
-  C --> D
-  style B fill:#969,stroke:#333,stroke-width:4px
+erDiagram
+    CUSTOMER ||--o{ ORDER : places
+    ORDER ||--|{ LINE-ITEM : contains
+    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses
 ```
+
+::right::
+
+- This is block diagrams
 
 ---
 layout: center
--- layouts/mermaid-left.vue
+++ layouts/mermaid-left.vue
@@ -1,7 +1,33 @@
+<!--
+  Usage:
+
+```md
+---
+layout: mermaid-left
+---
+
+# Mermaid
+
+```mermaid
+erDiagram
+    CUSTOMER ||--o{ ORDER : places
+    ORDER ||--|{ LINE-ITEM : contains
+    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses
+```
+
+::right::
+
+- This is block diagrams
+```
+-->
+
 <template>
-  <div class="slidev-layout mermaid-left">
-    <div class="my-auto w-full">
+  <div class="slidev-layout mermaid-left w-full h-full grid grid-cols-2">
+    <div class="col-left">
       <slot />
+    </div>
+    <div class="col-right">
+      <slot name="right"/>
     </div>
   </div>
 </template>
