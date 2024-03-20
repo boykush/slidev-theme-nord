a1ded36d30ce686a6ced201bac9f0c2949bfa11c
Taichi Kushiro <k.taichi0315@gmail.com>
2024-03-20 20:31:39 +0900

    fix mermaid theme

--- setup/mermaid.ts
+++ setup/mermaid.ts
@@ -1,7 +1,14 @@
 import { defineMermaidSetup } from '@slidev/types'
 
 export default defineMermaidSetup(() => {
+  const nord1Color = '#3B4252'
+  const nord3Color = '#4C566A'
   return {
-    theme: 'neutral',
+    theme: 'dark',
+    themeVariables: {
+      primaryColor: nord3Color,
+      mainBkg: nord3Color,
+      lineColor: nord1Color
+    }
   }
 })
